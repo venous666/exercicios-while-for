7d2aeb823389a819c6e4fbbc39be2d93bfd9d7f7
venous666 <99412095+venous666@users.noreply.github.com>
2022-05-05 12:51:49 -0300

    Add files via upload

---
+++ while.js
@@ -0,0 +1,56 @@
+//let n = 0
+//while (n != 3) {
+  //  console.log(n)
+    //n = Math.floor(Math.random() * 10)
+//}
+//let salario= 100
+//console.log("Salario inicial", salario)
+//while(salario < 10000){
+//salario += (salario * 1.15)
+//console.log("Novo Salario",salario.toFixed(2));
+
+
+//}
+
+//let b = 2
+//let exponencial = 1
+//console.log("Exponencial", exponencial)
+//while (exponencial < 32){
+  // exponencial = exponencial * b
+   //console.log(exponencial);
+    
+//}
+//console.log(exponencial);
+//console.log("exp:", 2**5)
+//console.log("exp:", Math.pow(2,5));
+
+//let salario = 2500
+//do{
+//salario = salario * 1.3
+//console.log(salario)
+//}  while (salario<2500) 
+
+//let a = 101
+//for(let i = 0; i < a; i++)
+//console.log(i);
+
+//let a = 101
+//for(let i = 0; i < a; i+=2)
+//console.log(i);
+
+//let salario = 100
+//let salario2 = 400
+//let salario3 = 500
+//let salario4 = 200
+//let salario5 = 300
+
+//let mediaSalarial = (salario + salario2 + salario3 + salario4 + salario5)/5
+//console.log(mediaSalarial)
+
+let n = 5
+let salario = 15000
+for(let i = 0; i < 5; i++){
+    console.log("salario", salario)
+    salario = salario / 2
+    console.log("Salario Atualizado", salario)
+}
